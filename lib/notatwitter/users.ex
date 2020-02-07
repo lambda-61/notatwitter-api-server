e83f9143230db8b3d7916852d3ceec9e1ce48b7c
yunmikun <a.paramonenko@elonsoft.ru>
2020-02-07 10:31:51 +0300

    Add model for replies

--- lib/notatwitter/users.ex
+++ lib/notatwitter/users.ex
@@ -3,6 +3,7 @@ defmodule Notatwitter.Users do
 
   alias Notatwitter.{Auth, User}
   alias Notatwitter.User.Post
+  alias Notatwitter.User.Post.Reply
 
   defdelegate list_users(), to: User.Manager, as: :list
   defdelegate find_user(id), to: User.Manager, as: :find
@@ -13,4 +14,13 @@ defmodule Notatwitter.Users do
   defdelegate find_post(post_id), to: Post.Manager, as: :find
   defdelegate create_post(user_id, attrs), to: Post.Manager, as: :create
   defdelegate update_post(post_id, attrs), to: Post.Manager, as: :update
+
+  defdelegate list_replies(post_id), to: Reply.Manager, as: :list
+  defdelegate find_reply(reply_id), to: Reply.Manager, as: :find
+
+  defdelegate create_reply(user_id, post_id, attrs),
+    to: Reply.Manager,
+    as: :create
+
+  defdelegate update_reply(reply_id, attrs), to: Reply.Manager, as: :update
 end
