dd934fb459137e456ec00451f1b9557c414d5a63
yunmikun <a.paramonenko@elonsoft.ru>
2020-02-07 10:42:13 +0300

    Add docs

--- lib/notatwitter_web/controllers/reply_controller.ex
+++ lib/notatwitter_web/controllers/reply_controller.ex
@@ -2,6 +2,7 @@ defmodule NotatwitterWeb.ReplyController do
   @moduledoc false
 
   use NotatwitterWeb, :controller
+  use PhoenixSwagger
 
   alias Notatwitter.User.AccessPolicy
   alias Notatwitter.Users
@@ -9,9 +10,28 @@ defmodule NotatwitterWeb.ReplyController do
 
   action_fallback NotatwitterWeb.ErrorController
 
+  swagger_path :index do
+    description("List replies of the post")
+    parameter(:post_id, :path, :integer, "")
+    CommonSwagger.requires_authorization()
+    response(200, "Ok", ReplySwagger.index_schema())
+    response(401, "Unauthorized")
+    response(404, "Post was not found")
+  end
+
   def index(conn, %{"post_id" => post_id}) do
     replies = Users.list_replies(post_id)
     render(conn, "index.json", replies: replies)
+  end
+
+  swagger_path :create do
+    description("Create a reply")
+    parameter(:post_id, :path, :integer, "")
+    CommonSwagger.requires_authorization()
+    ReplySwagger.creation_parameters()
+    response(201, "Created", ReplySwagger.show_schema())
+    response(404, "Post was not found")
+    response(422, "Unprocessable entity")
   end
 
   def create(conn, %{"post_id" => post_id} = params) do
@@ -22,6 +42,17 @@ defmodule NotatwitterWeb.ReplyController do
       |> put_status(:created)
       |> render("created.json", reply: reply)
     end
+  end
+
+  swagger_path :update do
+    description("Update a reply")
+    CommonSwagger.requires_authorization()
+    CommonSwagger.id_parameter()
+    ReplySwagger.creation_parameters()
+    response(200, "Ok", ReplySwagger.show_schema())
+    response(401, "Unauthorized")
+    response(404, "Post with such an id for current user does not exist")
+    response(422, "Unprocessable entity")
   end
 
   def update(conn, %{"id" => id} = params) do
